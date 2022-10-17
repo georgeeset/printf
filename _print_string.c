1f241905373428c228a361d196223decc535cfd3
George Esetevbe <georgeperfect4u@gmail.com>
2022-10-18 00:41:53 +0100

    printf and print_string function added

---
+++ _print_string.c
@@ -0,0 +1,19 @@
+#include "_printf.h"
+
+/**
+ * _print_string - prints string to screen
+ * @data: pointer to string to be printed
+ * Return: number of characters printed
+ * excluding list termination.
+ */
+
+int _print_string(char *data)
+{
+	int i;
+
+	for (i = 0; data[i] != '\0'; i++)
+	{
+		_putchar(data[i]);
+	}
+	return (i);
+}
---
+++ _printf.h
@@ -0,0 +1,9 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdarg.h>
+
+int _putchar(char c);
+int _print_string(char *data);
+int _printf(const char *format, ...);
+
+#endif
